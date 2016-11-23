72e378767a2b4639c205a6af40e5290d380b09f1
nalenal <enal.bidin@gmail.com>
2016-11-23 15:58:22 +0700

    runtime kok iseh yo,,,

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -1,16 +1,17 @@
 <?php
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
-use frontend\assets\AppAsset;
+//use yii\bootstrap\Nav;
+///use yii\bootstrap\NavBar;
+//use yii\widgets\Breadcrumbs;
+use frontend\assets\TokoAsset;
 use frontend\widgets\Alert;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-AppAsset::register($this);
+TokoAsset::register($this);
 ?>
+
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
@@ -23,54 +24,143 @@ AppAsset::register($this);
 </head>
 <body>
     <?php $this->beginBody() ?>
-    <div class="wrap">
-        <?php
-            NavBar::begin([
-                'brandLabel' => 'Shop',
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
-                ],
-            ]);
-            $itemsInCart = Yii::$app->cart->getCount();
-            $menuItems = [
-                ['label' => 'About', 'url' => ['/site/about']],
-                ['label' => 'Contact', 'url' => ['/site/contact']],
-                ['label' => 'My cart' . ($itemsInCart ? " ($itemsInCart)" : ''), 'url' => ['/cart/list']],
-            ];
-            /*if (Yii::$app->user->isGuest) {
-                $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-                $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-            } else {
-                $menuItems[] = [
-                    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                    'url' => ['/site/logout'],
-                    'linkOptions' => ['data-method' => 'post']
-                ];
-            }*/
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => $menuItems,
-            ]);
-            NavBar::end();
-        ?>
+  <!-- Navbar
+    ================================================== -->
+<div class="navbar navbar-fixed-top">
+              <div class="navbar-inner">
+                <div class="container">
+					<a id="logoM" href="index.html"><img src="assets/img/logo.png" alt="Bootsshop"/></a>
+                  <a data-target="#sidebar" data-toggle="collapse" class="btn btn-navbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </a>
+                  <div class="nav-collapse">
+                    <ul class="nav">
+					  <li class="active"><a href="index.html">Home	</a></li>
+					  <li class=""><a href="special_offer.html">Specials Offer</a></li>
+					  <li class=""><a href="normal.html">Delivery</a></li>
+					  <li class=""><a href="contact.html">Contact</a></li>
+					</ul>
+                    <form action="#" class="navbar-search pull-left">
+                     <input id="srchFld" type="text" placeholder="I'm looking for ..." class="search-query span5"/>
+                    </form>
+                    <ul class="nav pull-right">
+					<li class="dropdown">
+						<a data-toggle="dropdown" class="dropdown-toggle" href="#">Login <b class="caret"></b></a>
+						<div class="dropdown-menu">
+						<form class="form-horizontal loginFrm">
+						  <div class="control-group">								
+							<input type="text" class="span2" id="inputEmail" placeholder="Email">
+						  </div>
+						  <div class="control-group">
+							<input type="password" class="span2" id="inputPassword" placeholder="Password">
+						  </div>
+						  <div class="control-group">
+							<label class="checkbox">
+							<input type="checkbox"> Remember me
+							</label>
+							<button type="submit" class="btn pull-right">Sign in</button>
+						  </div>
+						</form>					
+						</div>
+					</li>
+					</ul>
+                  </div><!-- /.nav-collapse -->
+                </div>
+              </div><!-- /navbar-inner -->
+            </div>
+<!-- ======================================================================================================================== -->	
+<div id="mainBody" class="container">
+<header id="header">
+<div class="row">
+<div class="span12">
+	<a href="index.html"><img src="assets/img/logo.png" alt="Bootsshop"/></a>
+<div class="pull-right"> <br/>
+	<a href="product_summary.html"> <span class="btn btn-mini btn-warning"> <i class="icon-shopping-cart icon-white"></i> [ 3 ] </span> </a>
+	<a href="product_summary.html"><span class="btn btn-mini active">$155.00</span></a>
+	<span class="btn btn-mini">&pound;</span>
+	<span class="btn btn-mini">&euro;</span> 
+</div>
+</div>
+</div>
+<div class="clr"></div>
+</header>
+<!-- ==================================================Header End====================================================================== -->
 
-        <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-        </div>
-    </div>
+<?php
+		echo $content;
+?>
 
-    <footer class="footer">
-        <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-        <p class="pull-right"><?= Yii::powered() ?></p>
-        </div>
-    </footer>
+<!-- Footer ------------------------------------------------------ -->
+<hr class="soft">
+<div  id="footerSection">
+	<div class="row">
+		<div class="span3">
+			<h5>ACCOUNT</h6>
+			<a href="login.html">YOUR ACCOUNT</a>
+			<a href="login.html">PERSONAL INFORMATION</a> 
+			<a href="login.html">ADDRESSES</a> 
+			<a href="login.html">DISCOUNT</a>  
+			<a href="login.html">ORDER HISTORY</a>
+		 </div>
+		<div class="span3">
+			<h5>INFORMATION</h5>
+			<a href="contact.html">CONTACT</a>  
+			<a href="register.html">REGISTRATION</a>  
+			<a href="legal_notice.html">LEGAL NOTICE</a>  
+			<a href="tac.html">TERMS AND CONDITIONS</a> 
+			<a href="faq.html">FAQ</a>
+		 </div>
+		<div class="span3">
+			<h5>OUR OFFERS</h5>
+			<a href="#">NEW PRODUCTS</a> 
+			<a href="#">TOP SELLERS</a>  
+			<a href="special_offer.html">SPECIAL OFFERS</a>  
+			<a href="#">MANUFACTURERS</a> 
+			<a href="#">SUPPLIERS</a> 
+		 </div>
+		<div id="socialMedia" class="span3 pull-right">
+			<h5>SOCIAL MEDIA </h5>
+			<a href="#"><img width="60" src="assets/img/facebook.png" title="facebook"/></a>
+			<a href="#"><img width="60" src="assets/img/twitter.png" title="twitter"/></a>
+			<a href="#"><img width="60" src="assets/img/rss.png" title="rss"/></a>
+			<a href="#"><img width="60" src="assets/img/youtube.png" title="youtube"/></a>
+		 </div> 
+	 </div>
+	 <hr class="soft">
+	<p class="pull-right">&copy; Boot'sshop</p>
+</div><!-- /container -->
 
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster 
+    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    <script src="assets/js/jquery.js"></script>
+	<script src="assets/js/google-code-prettify/prettify.js"></script>
+    <script src="assets/js/application.js"></script>
+    <script src="assets/js/bootstrap-transition.js"></script>
+    <script src="assets/js/bootstrap-modal.js"></script>
+    <script src="assets/js/bootstrap-scrollspy.js"></script>
+    <script src="assets/js/bootstrap-alert.js"></script>
+    <script src="assets/js/bootstrap-dropdown.js"></script>
+    <script src="assets/js/bootstrap-tab.js"></script>
+    <script src="assets/js/bootstrap-tooltip.js"></script>
+    <script src="assets/js/bootstrap-popover.js"></script>
+    <script src="assets/js/bootstrap-button.js"></script>
+    <script src="assets/js/bootstrap-collapse.js"></script>
+    <script src="assets/js/bootstrap-carousel.js"></script>
+    <script src="assets/js/bootstrap-typeahead.js"></script>
+    <script src="assets/js/bootstrap-affix.js"></script>
+    <script src="assets/js/jquery.lightbox-0.5.js"></script>
+	<script src="assets/js/bootsshoptgl.js"></script>
+	-->
+	 <script type="text/javascript">
+    $(function() {
+        $('#gallery a').lightBox();
+    });
+    </script>
     <?php $this->endBody() ?>
 </body>
 </html>
